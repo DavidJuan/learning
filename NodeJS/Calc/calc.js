84609942eaa04e39f01880e413e59406d41813c9
David da Silva <davidjfds@gmail.com>
2019-09-26 17:29:15 -0300

    Aula01-Node.js

---
+++ NodeJS/Calc/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./func/soma");
+var SubFunc = require("./func/sub");
+var MultFunc = require("./func/mult");
+var DivFunc = require("./func/div");
+
+console.log(SomaFunc(10,5));
+console.log(SubFunc(10,5));
+console.log(MultFunc(10,5));
+console.log(DivFunc(10,5));
