4523ece46562f6aaa4df0215bbe267102c144499
DavidJuan <davidjfds@gmail.com>
2019-10-03 22:39:28 -0300

    review

--- NodeJS/test.js
+++ NodeJS/test.js
@@ -32,9 +32,9 @@ Usuario.create({
     idade: 28,
     email: 'teste@teste.com.br'
 })
-
-
-//alter user 'USER'@'localhost' identified with mysql_native_password by 'PASSWORD'
+//Usar //alter user 'USER'@'localhost' identified with mysql_native_password by 'PASSWORD'; // quando der erro abaixo no MySQL
+//Unhandled rejection SequelizeConnectionError: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+//alter user 'USER'@'localhost' identified with mysql_native_password by 'PASSWORD';
 
 // sequelize.authenticate().then(function () {
 //     console.log('Conectado com sucesso!!')    
