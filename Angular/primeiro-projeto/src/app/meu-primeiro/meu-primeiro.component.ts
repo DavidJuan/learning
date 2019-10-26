f5b1ae08708643c033f3c6dd299ca37698e2fe37
DavidJuan <davidjfds@gmail.com>
2019-10-26 12:36:57 -0300

    angular-init

---
+++ Angular/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template:`
+    <p>Este é o meu primeiro component em Angular 2</p><hr>
+    `
+})
+export class MeuPrimeiroComponent {}
