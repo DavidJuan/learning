0f4f7308cb2fd2fafee23d1882fdc790371b1c3b
DavidJuan <davidjfds@gmail.com>
2019-10-27 08:26:04 -0300

    include-C

--- Angular/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ Angular/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'meu-primeiro-component',
     template:`
-    <p>Este é o meu primeiro component em Angular 2</p><hr>
+    <p>Este é o meu primeiro component em Angular 2</p>
     `
 })
 export class MeuPrimeiroComponent {}
