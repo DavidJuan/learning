7addb6b4332a31ec0aedd562c5d0fada4c9a2eb3
DavidJuan <davidjfds@gmail.com>
2019-11-02 21:09:17 -0300

    routine maintenance

---
+++ C/C Fundamentos/1-HelloWorld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Fun��o principal do programa
+// void main(){
+
+//     //Imprime na tela
+//     printf("Oi mundo!");
+
+//     //Pausa o programa ap�s executar
+//     system("pause");
+
+// }
+
+void main(){
+    printf("oi mundo");
+    system("pause");
+    return 0;
+}
---
+++ C/C Fundamentos/2-ImprimirLerConcatenar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    printf("Mais uma pra coleção")
+    system("pause");
+}
+
+//     //Definindo uma vari�vel
+//     int a = 5, b;
+
+//     //Imprimindo a vari�vel "a"
+//     printf("%d", a);
+
+//     //Concatenando
+//     printf("\nO valor da var a eh: %d", a);
+
+//     //Mudando o valor de "a"
+//     a = 15;
+
+//     //Concatenando
+//     printf("\nO valor da var a eh: %d", a);
+
+//     //Lendo valores
+//     scanf("%d", &b);
+
+//     //Concatenando
+//     printf("\nO valor da var b eh: %d", b);
+
+//     //Pula linhas
+//     printf("\n");
+
+//     //Imprimindo
+//     printf("Uhuu");
+
+//     //Pausando
+//     system("pause");
+
+// }
